e5417b1a768bf482ca49738abb477f76c2ac99ed
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2022-06-29 22:29:17 +0530

    changes for jenkins

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
 
-ECHO 'Y' | Install-PackageProvider -Name NuGet -Scope CurrentUser
+Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
 ECHO 'Y' | Install-Package Microsoft.PowerShell.SDK -Version 7.0.6 -Type Module -Scope CurrentUser
 dotnet publish -c Release -r win10-x64   --self-contained True
