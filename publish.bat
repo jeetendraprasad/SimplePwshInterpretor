54ba363ed343754e60be16667f708b5729047f65
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2022-06-29 22:16:28 +0530

    changes for publish.ps1 file for automic build from jenkins

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
 
-
-Install-Package Microsoft.PowerShell.SDK -Version 7.0.6
-dotnet publish -c Release -r win10-x64   --self-contained True
+powershell -ExecutionPolicy RemoteSigned .\publish.ps1
+@REM Install-Package Microsoft.PowerShell.SDK -Version 7.0.6
+@REM dotnet publish -c Release -r win10-x64   --self-contained True
---
+++ publish.ps1
@@ -0,0 +1,4 @@
+
+Install-PackageProvider -Name NuGet -Scope CurrentUser
+Install-Package Microsoft.PowerShell.SDK -Version 7.0.6 -Type Module -Scope CurrentUser
+dotnet publish -c Release -r win10-x64   --self-contained True
